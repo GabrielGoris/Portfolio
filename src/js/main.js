03fec0dacf2df69e1d166b6eac0fc38489cdd998
Gabriel Goris <gabrieichans.gg@gmail.com>
2024-02-15 13:26:10 -0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+const modal = document.querySelector('.modal-container')
+
+function openModal() {
+    modal.classList.add('active')
+}
+
+function closeModal() {
+    modal.classList.remove('active')
+}
+
+
+
+
+
+
+
